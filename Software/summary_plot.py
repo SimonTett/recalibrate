88bebcf6889c5271aa09248520988638eb74d218
Sarah Sparrow <sarah.sparrow@oerc.ox.ac.uk>
2021-12-02 20:54:30 +0000

    Additional comments

--- Software/summary_plot.py
+++ Software/summary_plot.py
@@ -11,18 +11,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+# Define variables and plot colours
 data_cat=['raw','calibrated','calibrated no trend']
 variables=['TAS','NHD','PAP']
 cols=['RoyalBlue','SpringGreen','Gold']
 
+
+# Read in the summary information from a csv file to a dataframe and print
 data = pd.read_csv('../data/PR_data.csv',index_col=0)
-
-data_cat=['raw','calibrated','calibrated no trend']
-variables=['TAS','NHD','PAP']
-
 print(data)
-print(data.columns)
- # Set up the plot
+
+# Set up the plot
 font = {'family' : 'sans-serif',
         'size'   : 20}
 
@@ -37,19 +36,21 @@ ax.set_ylabel('Probability Ratio',fontsize=16)
 plt.setp(ax.get_xticklabels(),fontsize=16)
 plt.setp(ax.get_yticklabels(),fontsize=16)
 
-
-#l1=ax.semilogy(, basey=2
-
+# Plot the best estimate and 5-95% range for each variable for all three categories
 for iv,v in enumerate(variables):
     for c,cat in enumerate(data_cat):
         vals=data.loc[[v+' '+cat]]
-        print(vals['Best estimate'][0])
-        print(cols[c])
         ax.semilogy([(iv)*5+c+1.25,(iv)*5+c+0.75],[vals['Best estimate'][0],vals['Best estimate'][0]],basey=10,color=cols[c],lw=2,zorder=2)
         ax.fill_between([(iv)*5+c+1.25,(iv)*5+c+0.75],[vals['5%'][0],vals['5%'][0]], [vals['95%'][0],vals['95%'][0]],color=cols[c],alpha=0.3,zorder=1)
+
+# Plot the PR=1 line
 ax.semilogy([0,15],[1,1],basey=10,color='silver',ls='--',zorder=0)
+
+# Set the x axis range and tick labels
 ax.set_xlim(0,15)
 plt.xticks([2,7,12], variables)  # Set text labels.
+
+# Plot the legend and save the figure
 ax.legend(data_cat,fontsize="small")
 plt.tight_layout()
 fig.savefig('summary_plot.png')
